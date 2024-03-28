12e975f5d2200d238d7db169fdecc9eb9f28950e
TurkiKAlqou <turkikalqou@gmail.com>
2024-03-28 08:06:43 +0300

    testing react tailwind with vercel

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
